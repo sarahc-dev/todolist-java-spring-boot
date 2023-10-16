b15f07526dc59355a0431b043e96a9e58b7ff02f
sarahc-dev <sarahclements@hotmail.co.uk>
2023-10-16 15:00:24 +0100

    implement test controller

--- README.md
+++ README.md
@@ -6,5 +6,13 @@ I am going to create the Todolist API in Java using the Spring Boot framework.
 
 - Java
 - Spring Boot
+- Gradle
+- MongoDB
 
 ## Installation
+
+Need to have MongoDB running.
+
+## Process
+
+Project set up using Spring Initializr.
---
+++ src/main/java/com/example/todolist/TodoListController.java
@@ -0,0 +1,15 @@
+package com.example.todolist;
+
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+@RequestMapping("api/todos")
+public class TodoListController {
+
+    @GetMapping
+    public String index() {
+        return "ok";
+    }
+}
