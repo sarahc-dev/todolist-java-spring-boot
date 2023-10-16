100ebb6ed773924295251c5490a22704df33503a
sarahc-dev <sarahclements@hotmail.co.uk>
2023-10-16 20:45:43 +0100

    implemented mockMvc test from docs as test

--- src/main/java/com/example/todolist/TodoListController.java
+++ src/main/java/com/example/todolist/TodoListController.java
@@ -1,11 +1,13 @@
 package com.example.todolist;
 
+import org.springframework.web.bind.annotation.CrossOrigin;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
 @RestController
 @RequestMapping("api/todos")
+@CrossOrigin
 public class TodoListController {
 
     @GetMapping
